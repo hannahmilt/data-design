854094fc83939f78e9e96044aaec72de961b7218
Hannah Miltenberger <hmiltenberger@cnm.edu>
2020-03-16 15:58:47 -0600

    Created epic directory. Added persona.php, use-case-interaction-flow.php, user-story.php to epic. Added index.php file created links in index.php to files in directory Created html format for all files with assignment info in them Added photo Added type of user Added data-design.php and chart
    Added database.
    In database had to use heart instead of like

---
+++ sql/data-design.sql
@@ -0,0 +1,40 @@
+ALTER DATABASE hmiltenberger CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS heart;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile(
+	profileId BINARY(16) NOT NULL,
+	profileActivationToken CHAR(32),
+	profileDateOfBirth CHAR(6) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileGender VARCHAR(32),
+	profileHash CHAR(97) NOT NULL,
+	profileUsername VARCHAR(32) NOT NULL,
+	UNIQUE(profileEmail),
+	UNIQUE(profileUsername),
+	PRIMARY KEY(profileId)
+);
+
+CREATE TABLE post(
+   postCaption VARCHAR(140),
+   postDate DATETIME(6) NOT NULL,
+   postId BINARY(16) NOT NULL,
+   postLocation VARCHAR(32),
+   postPicture VARCHAR(32) NOT NULL,
+   postProfileId BINARY(16) NOT NULL,
+	INDEX(postProfileId),
+	FOREIGN KEY(postProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY(postId)
+);
+
+CREATE TABLE heart(
+   heartDate DATETIME(6) NOT NULL,
+   heartPostId BINARY(16),
+   heartProfileId BINARY(16),
+   FOREIGN KEY(heartProfileId) REFERENCES profile(profileId),
+   FOREIGN KEY(heartPostId) REFERENCES post(postId),
+   PRIMARY KEY(heartProfileId, heartPostId)
+);
+
